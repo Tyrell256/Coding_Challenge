3e7bbedaefb6525c70e5fb0d31b6e4451b578ead
Tyrell <tyrellcarter256@gmail.com>
2022-05-19 12:05:28 -0400

    Inital Changes

---
+++ cc.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int adding( ){
+
+    int var1;
+    int var2;
+
+    std::cout << "Enter a number \n";
+    std::cin >> var1;
+
+    std::cout << "Enter another number \n";
+    std::cin >> var2;
+
+    int anwser = var1 + var2;
+
+    std::cout << "The answer is " << anwser << "\n";
+
+    return anwser;
+}
+
+int main() {
+
+    adding();
+}
